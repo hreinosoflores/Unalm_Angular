f1d944fd1fe1da171859a2ee0d4aaaff03fa976a
hreinosoflores <55359120+hreinosoflores@users.noreply.github.com>
2021-07-19 17:20:10 -0500

    error detalles solucionado

--- src/app/components/pages/cursos/detalles/detalles.component.ts
+++ src/app/components/pages/cursos/detalles/detalles.component.ts
@@ -6,8 +6,7 @@ import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-detalles',
-  templateUrl: './detalles.component.html',
-  styleUrls: ['./detalles.component.css']
+  templateUrl: './detalles.component.html'
 })
 export class DetallesComponent implements OnInit {
 
